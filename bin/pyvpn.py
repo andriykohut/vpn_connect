c96291c92b579a56a8929f52520d6e1a3e0e9df8
andriykohut <kogut.andriy@gmail.com>
2014-05-15 14:13:41 +0300

    Add options for getting SoftToken password

--- bin/pyvpn.py
+++ bin/pyvpn.py
@@ -3,10 +3,11 @@ import sys
 import os
 import getpass
 from os.path import dirname, abspath
+from subprocess import Popen, PIPE
 libpath = os.path.join(dirname(dirname(abspath(__file__))), 'lib')
 sys.path.append(libpath)
 from optparse import OptionParser
-from colors import colors
+from colors import colors as col
 from config import get_config
 
 from softtoken import SoftToken
@@ -22,6 +23,11 @@ HUMAN_READABLE_NAMES = {
     'id': 'VPN cluster id',
     'wine': 'wine executable',
 }
+
+
+def copy_to_clipboard(text):
+    cmd = Popen(['xclip', '-selection', 'clipboard'], stdin=PIPE)
+    cmd.communicate(text.encode('utf-8'))
 
 
 def get_opts():
@@ -40,38 +46,59 @@ def get_opts():
     parser.add_option('--secret', help='VPN cluster secret')
     parser.add_option('--status', help='Display VPN status.',
                       action='store_true')
+    parser.add_option('--getpass', action='store_true',
+                      help='Print SoftToken password',)
+    parser.add_option('--copypass', action='store_true',
+                      help='Copy SoftToken password to clipboard')
     return parser.parse_args()[0].__dict__
 
 
 if __name__ == '__main__':
     opts = get_opts()
+
     if opts['status']:
         pid = VPNConnect.get_pid()
         if pid:
-            print 'vpnc running with PID %s' % colors.green(pid)
+            print 'vpnc running with PID %s' % col.green(pid)
         else:
-            print colors.red('vpnc is not running')
+            print col.red('vpnc is not running')
         sys.exit(0)
     config = get_config(os.path.expanduser((opts['config'])))
+
     for key, value in opts.items():
         if value:
             config[key] = value
+
+    if opts['getpass'] or opts['copypass']:
+        token = SoftToken(config['tuser'], config['consoleui'], config['wine'])
+        if not config['tpassword']:
+            prompt = 'SoftToken password for %s >>' % col.cyan(config['tuser'])
+            config['tpassword'] = getpass.getpass(prompt)
+        vpnpassword = token.get_password(config['tpassword'])
+        if opts['getpass']:
+            print col.yellow(vpnpassword)
+        else:
+            copy_to_clipboard(vpnpassword)
+            print col.green('Password copied to clipboard')
+        sys.exit(0)
+
     for unhuman, human in HUMAN_READABLE_NAMES.items():
         if not config[unhuman]:
             if ('password' in unhuman) or ('secret' in unhuman):
                 config[unhuman] = getpass.getpass(
-                    'Please enter %s: ' % colors.yellow(human)
+                    'Please enter %s: ' % col.yellow(human)
                 )
             else:
                 config[unhuman] = raw_input(
-                    'Please enter %s: ' % colors.yellow(human)
+                    'Please enter %s: ' % col.yellow(human)
                 )
+
     token = SoftToken(config['tuser'], config['consoleui'], config['wine'])
     vpnpassword = token.get_password(config['tpassword'])
     vpnc = VPNConnect(config['id'], config['gateway'],
                       config['secret'], config['vuser'])
     sudo_pass = getpass.getpass(
-        'Enter password for %s: ' % colors.cyan(getpass.getuser())
+        'Enter password for %s: ' % col.cyan(getpass.getuser())
     )
     if vpnc.vpn_connect(sudo_pass, vpnpassword) is True:
-        print colors.green('Connected!')
+        print col.green('Connected!')
